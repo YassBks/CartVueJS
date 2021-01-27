3d1b4e3fd6e1582ad5128da82ca2a8a7faf42789
yassine <boukassem.yassine@gmail.com>
2021-01-27 14:14:16 +0100

    case 0

--- src/components/CartExo.vue
+++ src/components/CartExo.vue
@@ -2,6 +2,7 @@
   <div class="cart-js">
     <div class="Panier">
 		<div class="titre"> Mon Panier</div>
+		<div class="vide" v-if="prixProduits === 0">Votre panier est vide</div>
 		<div v-for="(product,index) in products" :key="index">
 			<Produit :infos="product" @change="majPrix"/> 
 		</div>
@@ -12,9 +13,9 @@
 			<span class="sousTitre">Sous-total</span><span class="prix">{{ prixProduits }} €</span>
 		</div>
 		<div>
-			<span class="sousTitre">Livraison</span><span class="prix">{{prixLivraison}} €</span>
+			<span class="sousTitre">Livraison</span><span class="prix">{{ prixProduits === 0 ?  '0  €' : prixLivraison}}</span>
 		</div>
-		<select class="selectLivraison" name="livraison" id="livraison" v-model="prixLivraison">
+		<select class="selectLivraison" name="livraison" id="livraison" v-model="prixLivraison" :disabled="prixProduits === 0">
 			<option :value="3.00">Livraison standard en point relais (3,00 €)</option>
 			<option :value="3.95">Livraison standard (3,95 €)</option>
 			<option :value="10">Livraison 24h (10,00 €)</option>
@@ -43,7 +44,11 @@ export default {
   },
   computed: {
 		prixTotal () {
-			return this.prixProduits + this.prixLivraison;
+			if(this.prixProduits === 0) {
+				return 0;
+			} else {
+				return this.prixProduits + this.prixLivraison;
+			}
 		}
   },
   methods: {
@@ -76,7 +81,9 @@ export default {
     background-color: white;
 	min-width: 460px;
 }
-
+.vide {
+	text-align: center;
+}
 .titre {
 	margin: 0;
     font-size: 1.15em;    
